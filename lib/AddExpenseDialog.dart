78fed8da2e6dfffb0e9df18dda11708f459a7726
Anton Podchuvalov <podchuvalovvl@gmail.com>
2021-05-24 23:56:37 +1000

    Add ExpenseWidget and AddExpenseDialog

---
+++ lib/AddExpenseDialog.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AddExpenseDialog extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => AddExpenseDialogState();
+}
+
+class AddExpenseDialogState extends State<AddExpenseDialog> {
+  @override
+  Widget build(BuildContext context) {
+    return Text("Add expense dialog");
+  }
+}
---
+++ lib/ExpenseWidget.dart
@@ -0,0 +1,51 @@
+import 'package:expense_tracker/Expense.dart';
+import 'package:expense_tracker/AddExpenseDialog.dart';
+
+import 'package:flutter/material.dart';
+
+class ExpenseWidget extends StatefulWidget {
+  Expense _expense;
+
+  ExpenseWidget(this._expense);
+
+  @override
+  State<StatefulWidget> createState() => _ExpenseWidgetState(this._expense);
+}
+
+class _ExpenseWidgetState extends State<ExpenseWidget> {
+  Expense _expense;
+
+  _ExpenseWidgetState(this._expense);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      child: Card(
+        child: Row(
+          children: [
+            ListTile(
+              title: Text("${_expense.description}"),
+              leading: Icon(
+                Icons.attach_money,
+                color: Colors.blue[500],
+              ),
+            ),
+            Divider(),
+            ListTile(
+              title: Text("${_expense.price}"),
+              subtitle: Text("${_expense.date.toString()}"),
+            )
+          ],
+        ),
+      ),
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) {
+              return AddExpenseDialog();
+            })
+        ).then((value) => setState(() {}));
+      },
+    );
+  }
+}
